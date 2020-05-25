b5086324d1e62a2bff97596d6e394fd96ba7d228
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-05-25 16:07:12 +0300

    dffdsgsd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Layout from './components/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 
 
 function App() {
   return (
     <div >
       <Layout>
-        <BurgerBuilder> </BurgerBuilder>
+        <BurgerBuilder> 
+         
+        </BurgerBuilder>
       </Layout>
  
     </div>
