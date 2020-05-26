5844537dcb24733f728b37dd2dfa55f76a379506
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-05-26 11:57:09 +0300

    kjfjl

--- my-app/src/components/Burger/Burger.js
+++ my-app/src/components/Burger/Burger.js
@@ -7,13 +7,17 @@ const burger = (props)=>{
     let transformedIngredients = Object.keys(props.ingredients).map(igKey=>{
         return[...Array( props.ingredients[igKey])].map((_,i)=>{
             return <BurgerIngredient key={igKey + i} type={igKey} />;
-        }).reduce((arr, cur)=>{
-            return arr.concat(cur)
-        },[]);
+        })
+    }).reduce((arr, cur)=>{
+        return arr.concat(cur)
+    },[]);
 
-        
-    });
-console.log(transformedIngredients);
+    console.log(transformedIngredients.length,transformedIngredients);
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients</p>;
+    }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
