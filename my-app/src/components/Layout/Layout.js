8152b12e334a35606ec1b47c2e87fb84ed5c2f1f
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-05-28 19:24:18 +0300

    awfwafw

--- my-app/src/components/Layout/Layout.js
+++ my-app/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css'
+import ToolBar from '../Navigation/ToolBar/ToolBar'
 import Aux from '../../hoc/Auxiliary'
 
 const layout =(props) =>{
     return ( 
     <React.Fragment>
-        <div>Toolbar, SideBar, Backdrop</div>
+        <ToolBar/>
         <main className={classes.Content}>
             {props.children}
         </main>
