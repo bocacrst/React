77517d5ec1ba83fae2de075aed9d096c153c22eb
bocacrst <61462231+bocacrst@users.noreply.github.com>
2020-05-29 11:54:32 +0300

    gghjllgvb

--- my-app/src/components/Layout/Layout.js
+++ my-app/src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import classes from './Layout.module.css'
 import ToolBar from '../Navigation/ToolBar/ToolBar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import Aux from '../../hoc/Auxiliary'
+
 
 const layout =(props) =>{
     return ( 
     <React.Fragment>
         <ToolBar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
--- my-app/src/components/Navigation/ToolBar/ToolBar.js
+++ my-app/src/components/Navigation/ToolBar/ToolBar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import classes from './ToolBar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolBar = (props)=>(
     <header className={classes.ToolBar}>
         <div>MENU</div>
         <Logo/>
-        <nav>
-            ...
-        </nav>
+        <NavigationItems/>
     </header>
 );
 
